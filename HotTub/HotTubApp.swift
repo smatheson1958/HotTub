b536c4ca1c1ffb0fb9b435561041716fbea999de
scottmatheson58 <scott@matheson.it>
2026-04-03 18:13:13 +0100

    Initial Commit

---
+++ HotTub/HotTubApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotTubApp.swift
+//  HotTub
+//
+//  Created by scott matheson on 03/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct HotTubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
